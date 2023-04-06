42adffc68285c8662f5bf5adb19911f15f9459b5
wantonmeeman <tokyo.yu.96@gmail.com>
2023-04-07 00:21:43 +0800

    Test Function Created!

---
+++ index.js
@@ -0,0 +1,8 @@
+var app = require('./src/app.js');
+
+const PORT = 3000
+const IPADDR = '127.0.0.1'
+
+var server = app.listen(PORT, IPADDR, function () {
+    console.log(`Web App Hosted at http://${IPADDR}:${PORT}`);
+});
---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+
+const fs = require('fs')
+
+const cors = require('cors')
+
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+app.use(bodyParser.json()); //parse appilcation/json data
+app.use(urlencodedParser); //Check insert user.
+app.use(cors())
+
+app.get('/test',(req,res)=>{
+    console.log(req)
+    res.status(200).end()
+})
+
+module.exports = app
