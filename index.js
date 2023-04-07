8eca9b3fbd88d499a7464899f10bffee83fea718
wantonmeeman <tokyo.yu.96@gmail.com>
2023-04-07 17:29:53 +0800

    Revert Prev

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('./src/app.js');
 
 const PORT = 3000
-const IPADDR = '192.168.1.235'
+const IPADDR = '127.0.0.1cd '
 
 var server = app.listen(PORT, IPADDR, function () {
     console.log(`Web App Hosted at http://${IPADDR}:${PORT}`);
