402b450b64f7adeb7bea5da00bea6d29ffd49dc7
wantonmeeman <tokyo.yu.96@gmail.com>
2023-05-04 20:22:49 +0800

    Added Basic CRUD

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -12,4 +12,25 @@ app.use(bodyParser.json()); //parse appilcation/json data
 app.use(urlencodedParser); //Check insert user.
 app.use(cors())
 
+app.get('/', (req, res) => {
+    console.log(req)
+    res.status(200).end()
+})
+
+app.post('/', (req, res) => {
+    console.log(req)
+    res.status(200).end()
+})
+
+app.put('/', (req, res) => {
+    console.log(req)
+    res.status(200).end()
+})
+
+app.delete('/', (req, res) => {
+    console.log(req)
+    res.status(200).end()
+})
+
+
 module.exports = app
